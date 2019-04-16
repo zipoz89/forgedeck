945b4e3027c9ab899f1b5e27862e446b609d9a91
zipoz89 <31932780+zipoz89@users.noreply.github.com>
2019-04-16 23:30:52 +0200

    Add files via upload

--- forgedeck 1.2/ForgeDeck(compendium)/content.js
+++ forgedeck 1.2/ForgeDeck(compendium)/content.js
@@ -1,7 +1,12 @@
 
-
 chrome.runtime.onMessage.addListener(function (request) {
 	var one = window.location;
-	var res = one.toString().split("/");
-	window.open("https://keyforge-compendium.com/decks/" + res[4], "_blank");
+	var check = one.toString().split("/");
+	if(check[2] == "www.keyforgegame.com")
+	{
+		var res = one.toString().split("/");
+		window.open("https://keyforge-compendium.com/decks/" + res[4], "_blank");
+	}
+	else
+	window.open("https://keyforge-compendium.com/", "_blank");
 })
